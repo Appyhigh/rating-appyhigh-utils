b5e075d209ad4755910ea7080e1ca4b646b5a8cb
avirajrsingh <aviraj.rsingh@gmail.com>
2021-08-20 16:27:40 +0530

    Add methods for changing text color and image tint

--- app/src/main/java/com/lodestar/inappreviewmodule/MainActivity.kt
+++ app/src/main/java/com/lodestar/inappreviewmodule/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lodestar.inappreviewmodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.appyhigh.in_app_review.EmojiRatingBar
@@ -20,10 +21,18 @@ class MainActivity : AppCompatActivity() {
         val ratingValue = findViewById<EmojiRatingBar>(R.id.rating_bar)
         val ratingTitle = findViewById<TextView>(R.id.ratingTitleTv)
         val ratingSubtitle1 = findViewById<TextView>(R.id.ratingMessageTv1)
+        val imgDown = findViewById<ImageView>(R.id.imgDown)
+        val imgUp = findViewById<ImageView>(R.id.imgUp)
+        val ratingLike = findViewById<TextView>(R.id.ratingLike)
+        val ratingDislike = findViewById<TextView>(R.id.ratingDislike)
 
         inapp.changeBackgroundOfView(rateNow, resources.getColor(R.color.colorPrimary))
         inapp.changeTextOfView(ratingTitle, "Enjoying ${getString(R.string.app_name)}?")
         inapp.changeTextOfView(ratingSubtitle1, "Rate us to share your feedback!")
+        inapp.changeTextColorOfView(ratingLike, resources.getColor(R.color.colorPrimary))
+        inapp.changeTextColorOfView(ratingDislike, resources.getColor(R.color.colorPrimary))
+        inapp.changeTintOfArrow(imgDown,resources.getColor(R.color.colorPrimary))
+        inapp.changeTintOfArrow(imgUp,resources.getColor(R.color.colorPrimary))
 
         rateNow.setOnClickListener {
             inapp.startInAppReviewFlow(ratingValue.rating, 4.0f)
